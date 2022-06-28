e254c5d91ec339d417adf6f367406f0ae230ce1a
Susan Brown <brown684408@gmail.com>
2022-06-28 15:54:02 -0500

    edit header date with moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 
 //display current date in header
-var rightNow = new Date();
-var numHours = 1
+var rightNow = moment().format("dddd, DD MMMM YYYY");
+console.log(rightNow);
 
 let header = document.querySelector("#currentDay");
 header.innerText = rightNow;
+
+var numHours = 1
+
+// var rightNow = new Date();
+
+
+
+
+// dateDay.textContent = currentDate.format("MMM DD, YYYY - hh:mm:ss a");
+
+// console.log(currentDate);
 
 function classes(i) {
 
