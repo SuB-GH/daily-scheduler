04e13b380a0a14a96fb5ae692a1ef8a8dc612aad
Susan Brown <brown684408@gmail.com>
2022-06-28 15:09:31 -0500

    add time blocks and submit buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 
 //display current date in header
 var rightNow = new Date();
+var numHours = 1
 
 let header = document.querySelector("#currentDay");
 header.innerText = rightNow;
+
+function classes(i) {
+
+    for(var i=0; i<numHours; i++) {
+        console.log("hi"+i);
+    }
+}
+
+classes();
